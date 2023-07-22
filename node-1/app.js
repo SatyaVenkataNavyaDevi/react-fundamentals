515d48477bd23db5c721dfde80a149ee8eb8f22a
Satya Venkata Navya Devi <140080660+SatyaVenkataNavyaDevi@users.noreply.github.com>
2023-07-22 07:17:42 +0000

    Added React Fundamentals-1

---
+++ node-1/app.js
@@ -0,0 +1,5 @@
+const title = React.createElement("h1",{className:"heading"},"This is Satya ! Hello React!!!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(title);
